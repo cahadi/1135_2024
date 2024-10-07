87b264045607a5d3a789bb737a3ac83d1789e1ca
cahadi <ap357775462534@gmail.com>
2024-10-07 18:15:09 +1000

    Вывод списка и чтение по одной записи

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+include_once './inc/function.php';
+
+include './template/frontend/partials/head.php';
+include './template/frontend/partials/breadcrumbs.php';
+include './template/frontend/partials/blog-list.php';
+include './template/frontend/partials/footer.php';
